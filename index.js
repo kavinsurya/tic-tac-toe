29af1ab78e2e9bcbb5485ae9878c97cea7e627ac
kavinsurya <kavinsurya002@gmail.com>
2021-07-14 10:47:03 +0530

    Socket and game initialization

--- index.js
+++ index.js
@@ -1,12 +1,71 @@
 const express = require("express");
 const app = express();
-const http  = require('http').Server(app);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const port = 5050;
 
-app.get('/', function (req, res) {
-  console.log('kavin');
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
+var players = {},unmatched;
+
+function joinGame(socket) {
+  // Add the player
+  players[socket.id] = {
+    opponent: unmatched,
+    symbol: "X",
+    socket: socket,
+  };
+
+  if (unmatched) {
+    players[socket.id].symbol = "O";
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  } else {
+    unmatched = socket.id;
+  }
+}
+
+// Returns the opponent socket ID
+function getOpponent(socket) {
+  if (!players[socket.id].opponent) {
+    return;
+  }
+  return players[players[socket.id].opponent].socket;
+}
+
+io.on("connection", function (socket) {
+  joinGame(socket);
+
+  // Once the opponent have joined game begins
+  if (getOpponent(socket)) {
+    socket.emit("game.begin", {
+      symbol: players[socket.id].symbol,
+    });
+    getOpponent(socket).emit("game.begin", {
+      symbol: players[getOpponent(socket).id].symbol,
+    });
+  }
+  //Event is executed when move is made
+  socket.on("make.move", function (data) {
+    if (!getOpponent(socket)) {
+      return;
+    }
+    console.log("Move made by : ", data);
+    socket.emit("move.made", data);
+    getOpponent(socket).emit("move.made", data);
+  });
+
+  //Event is executed when move is made when opponent leaves
+  socket.on("disconnect", function () {
+    if (getOpponent(socket)) {
+      getOpponent(socket).emit("opponent.left");
+    }
+  });
+});
 
 http.listen(port, () => console.log(`listening on port ${port}`));
